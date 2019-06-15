0912db700cc9e09e95a4949d900c7a3fe56ce0e6
zhutianning <tianning.zhu@kcl.ac.uk>
2019-06-15 21:45:29 +0100

    Avoid Redundant Interactions

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const pubsub = new PubSub({ blockchain });
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -51,5 +50,7 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`);
 
-    syncChains();
+    if (PORT !== DEFAULT_PORT) {
+        syncChains();
+    }
 });
