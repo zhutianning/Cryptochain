806024aff878dd9edad2783062f2f9c3ed1c799d
zhutianning <tianning.zhu@kcl.ac.uk>
2019-06-28 17:12:37 +0100

    broadcast transactions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet(); // you can use wallet.createTransaction  method to create transaction anytime you want.
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -50,6 +50,8 @@ app.post('/api/transact', (req, res) => {
     }
 
     transactionPool.setTransaction(transaction);
+
+    pubsub.broadcastTransaction(transaction);
 
     res.json({ type: 'success', transaction });
 });
