485d4e33c5278f8778e8f7322419ef8e222c53cd
zhutianning <tianning.zhu@kcl.ac.uk>
2019-07-01 14:24:28 +0100

    Transaction Miner Class

---
+++ app/transaction-miner.js
@@ -0,0 +1,19 @@
+class TransactionMiner {
+    constructor({}) {
+
+    }
+
+    mineTransactions() {
+        // get the transaction pool's valid transactions
+
+        // generate the miner's reward
+
+        // add a block consisting of these transactions to the blockchain
+
+        // broadcast the updated blockchain
+
+        // clear the pool
+    }
+}
+
+module.exports = TransactionMiner;
