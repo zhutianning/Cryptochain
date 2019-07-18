3ec4122e859fed2aa3eb6611d3782de7068a4bad
zhutianning <tianning.zhu@kcl.ac.uk>
2019-07-18 22:36:03 +0100

    stylize the frontend

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -23174,7 +23174,8 @@ function (_Component) {
       console.log('this.state', this.state);
       return _react.default.createElement("div", null, _react.default.createElement("h3", null, "Blocks"), this.state.blocks.map(function (block) {
         return _react.default.createElement("div", {
-          key: block.hash
+          key: block.hash,
+          className: "Block"
         }, block.hash);
       }));
     }
@@ -23185,7 +23186,9 @@ function (_Component) {
 
 var _default = Blocks;
 exports.default = _default;
-},{"react":"../../../../node_modules/react/index.js"}],"components/App.js":[function(require,module,exports) {
+},{"react":"../../../../node_modules/react/index.js"}],"assets/logo.jpg":[function(require,module,exports) {
+module.exports = "/logo.ed8fc6de.jpg";
+},{}],"components/App.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -23196,6 +23199,8 @@ exports.default = void 0;
 var _react = _interopRequireWildcard(require("react"));
 
 var _Blocks = _interopRequireDefault(require("./Blocks"));
+
+var _logo = _interopRequireDefault(require("../assets/logo.jpg"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -23261,7 +23266,14 @@ function (_Component) {
       var _this$state$walletInf = this.state.walletInfo,
           address = _this$state$walletInf.address,
           balance = _this$state$walletInf.balance;
-      return _react.default.createElement("div", null, _react.default.createElement("div", null, "Welcome to the blockchain ..."), _react.default.createElement("div", null, "Address: ", address), _react.default.createElement("div", null, "Balance: ", balance), _react.default.createElement("br", null), _react.default.createElement(_Blocks.default, null));
+      return _react.default.createElement("div", {
+        className: "App"
+      }, _react.default.createElement("img", {
+        className: "logo",
+        src: _logo.default
+      }), _react.default.createElement("br", null), _react.default.createElement("div", null, "Welcome to the blockchain ..."), _react.default.createElement("br", null), _react.default.createElement("div", {
+        className: "WalletInfo"
+      }, _react.default.createElement("div", null, "Address: ", address), _react.default.createElement("div", null, "Balance: ", balance)), _react.default.createElement("br", null), _react.default.createElement(_Blocks.default, null));
     }
   }]);
 
@@ -23271,7 +23283,79 @@ function (_Component) {
 var _default = App; // share files
 
 exports.default = _default;
-},{"react":"../../../../node_modules/react/index.js","./Blocks":"components/Blocks.js"}],"index.js":[function(require,module,exports) {
+},{"react":"../../../../node_modules/react/index.js","./Blocks":"components/Blocks.js","../assets/logo.jpg":"assets/logo.jpg"}],"../../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"../../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"index.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"../../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
@@ -23280,12 +23364,14 @@ var _reactDom = require("react-dom");
 
 var _App = _interopRequireDefault(require("./components/App"));
 
+require("./index.css");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // this function is the key to insert react into frontend
 //allow us to dynamically insert react components into the HTML document
 (0, _reactDom.render)(_react.default.createElement(_App.default, null), document.getElementById('root'));
-},{"react":"../../../../node_modules/react/index.js","react-dom":"../../../../node_modules/react-dom/index.js","./components/App":"components/App.js"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"react":"../../../../node_modules/react/index.js","react-dom":"../../../../node_modules/react-dom/index.js","./components/App":"components/App.js","./index.css":"index.css"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -23313,7 +23399,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53371" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51553" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom'; // this function is the key to insert react into frontend
 //allow us to dynamically insert react components into the HTML document
 import App from './components/App';
+import './index.css';
 
 render( 
     <App />,
