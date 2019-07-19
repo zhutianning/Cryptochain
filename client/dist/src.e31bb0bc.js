028b78addd6cd2e25a2036d87e2294db4ae34cc9
zhutianning <tianning.zhu@kcl.ac.uk>
2019-07-19 16:15:06 +0100

    make a block component with props

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -23102,7 +23102,7 @@ if ("development" === 'production') {
 } else {
   module.exports = require('./cjs/react-dom.development.js');
 }
-},{"./cjs/react-dom.development.js":"../../../../node_modules/react-dom/cjs/react-dom.development.js"}],"components/Blocks.js":[function(require,module,exports) {
+},{"./cjs/react-dom.development.js":"../../../../node_modules/react-dom/cjs/react-dom.development.js"}],"components/Block.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -23111,6 +23111,74 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 
 var _react = _interopRequireWildcard(require("react"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+//This is the children file of Blocks.js 
+var Block =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Block, _Component);
+
+  function Block() {
+    _classCallCheck(this, Block);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Block).apply(this, arguments));
+  }
+
+  _createClass(Block, [{
+    key: "render",
+    value: function render() {
+      var _this$props$block = this.props.block,
+          timestamp = _this$props$block.timestamp,
+          hash = _this$props$block.hash,
+          data = _this$props$block.data;
+      var hashDisplay = "".concat(hash.substring(0, 15), "...");
+      var stringifiedData = JSON.stringify(data);
+      var dataDisplay = stringifiedData.length > 35 ? "".concat(stringifiedData.substring(0, 35), "...") : stringifiedData;
+      return _react.default.createElement("div", {
+        className: "Block"
+      }, _react.default.createElement("div", null, "Hash: ", hashDisplay), _react.default.createElement("div", null, "Timestamp: ", new Date(timestamp).toLocaleString()), _react.default.createElement("div", null, "Data: ", dataDisplay));
+    }
+  }]);
+
+  return Block;
+}(_react.Component);
+
+;
+var _default = Block;
+exports.default = _default;
+},{"react":"../../../../node_modules/react/index.js"}],"components/Blocks.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _Block = _interopRequireDefault(require("./Block"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
@@ -23173,10 +23241,10 @@ function (_Component) {
     value: function render() {
       console.log('this.state', this.state);
       return _react.default.createElement("div", null, _react.default.createElement("h3", null, "Blocks"), this.state.blocks.map(function (block) {
-        return _react.default.createElement("div", {
+        return _react.default.createElement(_Block.default, {
           key: block.hash,
-          className: "Block"
-        }, block.hash);
+          block: block
+        });
       }));
     }
   }]);
@@ -23186,7 +23254,7 @@ function (_Component) {
 
 var _default = Blocks;
 exports.default = _default;
-},{"react":"../../../../node_modules/react/index.js"}],"assets/logo.jpg":[function(require,module,exports) {
+},{"react":"../../../../node_modules/react/index.js","./Block":"components/Block.js"}],"assets/logo.jpg":[function(require,module,exports) {
 module.exports = "/logo.ed8fc6de.jpg";
 },{}],"components/App.js":[function(require,module,exports) {
 "use strict";
@@ -23399,7 +23467,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51553" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52093" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
